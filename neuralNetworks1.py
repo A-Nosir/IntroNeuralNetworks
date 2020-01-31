1037a3d034724969c5a517943391c7b0b1f86e64
Amr Nosir <amr.nosir_96@hotmail.com>
2020-01-30 20:22:19 -0500

    Add files via upload

--- neuralNetworks1.py
+++ neuralNetworks1.py
@@ -4,15 +4,8 @@
 # For example, here's several helpful packages to load in 
 
 import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+#import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
-
-import os
-for dirname, _, filenames in os.walk('/kaggle/input'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
 
 # Any results you write to the current directory are saved as output.
 
